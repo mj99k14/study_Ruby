ecabd325588cdbf67598cfc51653ab532896f7a9
mj99k14 <ekfkawnl644@gmail.com>
2025-09-02 00:06:48 +0900

    method

---
+++ default.rb
@@ -0,0 +1,39 @@
+# def greet(name ="민정",message ="ㅎㅎ")
+#     puts "하이, #{name}"
+#     puts message
+# end
+# greet
+
+# def print_info(name:,age:)
+#     puts"#{name}씨#{age}살"
+# end
+
+# print_info(age:27,name:"민정")
+
+# def format_profile(name:, age:, hobby:"読書")
+#     puts "#{name}さん（#{age}歳）の趣味は#{hobby}です"
+# end
+
+
+# format_profile(name: "kmj", age: 34)
+# format_profile(name: "kmj", age: 34, hobby:"料理")
+
+# def sum(*numbers)
+#     numbers.sum
+
+# end
+# puts sum[4,5]
+
+# def greet (message, *names)
+#     names.each do|name|
+#         puts "#{name},#{message}"
+#     end
+# end
+
+# greet("안녕","민정","워","승")
+
+def divide(a,b)
+    a / b 
+end
+
+puts divi
