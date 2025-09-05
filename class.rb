e84bfb55b68f4d4da4330f593311c1fca90f921c
mj99k14 <ekfkawnl644@gmail.com>
2025-09-05 21:59:19 +0900

    initialize( 생성자)

--- class.rb
+++ class.rb
@@ -1,57 +1,15 @@
-# class Dog
-#     def set_name(name)
-#         @name = name
-#     end
-
-#     def bark
-#         puts"#{@name} 왕왕"
-#     end
-# end
-
-
-
-# pochi = Dog.new
-
-# # pochi.set_name("alal")
-# puts pochi.inspect
-
-
-# # shiro = Dog.new
-# # shiro = set_name("시로")
-# # shiro.brak
-
-
-# class Cat
-#     def set_name(name)
-#         @name = name
-
-#     end
-#     def set_age(age)
-#         @age = age
-#     end
-#     def introduce(name,age)
-#         puts "이름 : #{name}나이:#{age}"
-#     end
-
-# end
-
-# cat = Cat.new
-
-# cat.introduce("kmj",30)
-
-class Calculator
-    def calculate_total(price)
-        @tax = 0.1
-        total = price * (1 + @tax)
-        puts "세금 금액 #{total}"
+class Character
+    def initialize
+        @name = "이름없음"
+        @hp = 100
+        puts "뭔가만들었음"
     end
 
-    def show_tax
-        puts " 소비세 #{@tax}"
+    def show_status
+        puts "이름:#{@name}"
+        puts"hp: #{@hp}"
     end
-
 end
 
-calc = Calculator.new
-calc.calculate_total(1000)
-calc.show_tax
+hero = Character.new
+hero.show_status
